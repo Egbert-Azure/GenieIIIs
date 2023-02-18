0a6b67ebf51ed0899d8ed0c48a6fc0211e31b232
Egbert-Azure <55332675+Egbert-Azure@users.noreply.github.com>
2023-02-18 11:47:49 -0800

    grafik.inc

---
+++ Utilities src/TurboPascal/erax.pas
@@ -0,0 +1,82 @@
+{$U-,C-,R-}
+
+{
+
+  *
+  *  ERAX.PAS
+  *
+  *  komfortables Loeschen von Dateien
+  *  nach Eingabe einer Dateimaske werden passende Dateien abgefragt
+  *
+  *
+  *  (c)  26.2.1991  -  FuzzySoft
+  *
+
+}
+
+
+{$I CPM31.INC  -  Zugriff auf Betriebssystem CP/M 3.1   }
+
+
+type
+  Str80= string[80];
+
+
+var
+  f:     file;
+  fName: cpmStr12;
+  Dir:   cpmDir;
+
+
+function GetFileName( var fName: cpmStr12 ): boolean;
+begin
+  writeln;
+  write( 'ERAX> ' );
+  readln( fName );
+  while( pos( ' ', fName ) = 1 ) do
+    delete( fName, 1, 1 );
+  GetFileName:= ( fName <> '' );
+end;
+
+
+function Erlaubnis: boolean;
+var
+  c: char;
+begin
+  read( Kbd, c );
+  if( c = ^c ) then begin
+    writeln( '^C' );
+    halt;
+  end;
+  Erlaubnis:= ( c in ['J','j','Y','y',#13] );
+end;
+
+
+procedure InitProgram;
+begin
+  writeln;
+  writeln( '--------------------' );
+  writeln( 'ERAX 1.0' );
+  writeln( 'Loeschen von Dateien' );
+  writeln( '--------------------' );
+end;
+
+
+begin
+  InitProgram;
+  while( GetFileName( fName )) do begin
+    newDirectory( Dir, fName );
+    while( Dir <> nil ) do begin
+      fName:= nextFileName( Dir );
+      write( ' - ', fName, '':( 15 - length( fName )));
+      if( Erlaubnis ) then begin
+        assign( f, fName );
+        erase( f );
+        Write( '#' );
+      end;
+      writeln;
+    end;
+  end;
+end.
+
+
